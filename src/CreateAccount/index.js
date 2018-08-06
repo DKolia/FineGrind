1531a96d050a5299e18291bb702e1aedfe24a8a9
David Kolia <Dkolia@icloud.com>
2018-08-06 13:18:08 -0500

    Added base functionality to Login and Create Account pages, need to string to DB

--- src/CreateAccount/index.js
+++ src/CreateAccount/index.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 
-function CreateAccount() {
-  return (
-    <div>
-      <img src="./Images/times-circle-regular.svg"></img>
-      <h1>Begin posting FindGrind jobs.</h1>
-      <p>By signing up for our free service, you will be able to post jobs in your area. Please fill out the below to begin.</p>
-      <p>Please enter your email address, a unique password, and confirm the password.</p>
-      <form>
-        <input type="text" id="email" placeholder="enter email" />
-        <input type="password" id="password" placeholder="enter password" />
-        <input type="password" id="confirm password" placeholder="confirm password" />
-        <button type="subtmit">Submit</button>
-      </form>
-    </div>
-  )
+class Login extends Component() {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      confirmpassword: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.login(this.state.email);
+  }
+
+  render() {
+    return (
+      <div>
+        <img src="./Images/times-circle-regular.svg"></img>
+        <h1>Begin posting FindGrind jobs.</h1>
+        <p>By signing up for our free service, you will be able to post jobs in your area. Please fill out the below to begin.</p>
+        <p>Please enter your email address, a unique password, and confirm the password.</p>
+        <form>
+          <input type="text" id="email" onChange={this.handleChange} value={this.state.email} placeholder="enter email" />
+          <input type="password" id="password" onChange={this.handleChange} value={this.state.password} placeholder="enter password" />
+          <input type="password" id="confirm password" placeholder="confirm password" />
+          <input type="submit" name="submit" />
+        </form>
+      </div>
+    )
+  }
+
 }
 
 export default CreateAccount;
--- src/Login/index.js
+++ src/Login/index.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 
-function Login() {
-  return (
-    <div>
-      <img src="./Images/times-circle-regular.svg"></img>
-      <h1>Please Login to post a FineGrind</h1>
-      <p>Login below to begin posting a FineGrind opportunity. If you don't have an account yet, please click <a href="#">here</a> to create one for free.</p>
-      <form>
-        <input type="text" id="email" placeholder="enter email" />
-        <input type="password" id="password" placeholder="enter password" />
-        <button type="subtmit">Submit</button>
-      </form>
+class Login extends Component {
+  constructor(){
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.login(this.state.email);
+  }
+
+  render(){
+    return (
+      <div>
+        <img src="./Images/times-circle-regular.svg"></img>
+        <h1>Please Login to post a FineGrind</h1>
+        <p>Login below to begin posting a FineGrind opportunity. If you don't have an account yet, please click <a href="#">here</a> to create one for free.</p>
+        <form>
+          <input type="text" id="email" onChange={this.handleChange} value={this.state.email} placeholder="enter email" />
+          <input type="password" id="password" onChange={this.handleChange} value={this.state.password} placeholder="enter password" />
+          <input type="submit" name="submit" />
+        </form>
     </div>
-  )
+    )
+  }
 }
 
 export default Login;
