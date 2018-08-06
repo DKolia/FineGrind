bbc8bf0026b571c7505ddeef9e81868c0af80e16
David Kolia <Dkolia@icloud.com>
2018-08-06 11:59:10 -0500

    Finished basic layout of login page

--- src/Login/index.js
+++ src/Login/index.js
@@ -3,7 +3,14 @@ import React, { Component } from "react";
 function Login() {
   return (
     <div>
-      <h1>OMG this is the login page!</h1>
+      <img src="./Images/times-circle-regular.svg"></img>
+      <h1>Please Login to post a FineGrind</h1>
+      <p>Login below to begin posting a FineGrind opportunity. If you don't have an account yet, please click <a href="#">here</a> to create one for free.</p>
+      <form>
+        <input type="text" id="email" placeholder="enter email" />
+        <input type="password" id="password" placeholder="enter password" />
+        <button type="subtmit">Submit</button>
+      </form>
     </div>
   )
 }
