232b79b7842e559df71643030a853662cc27f100
David Kolia <Dkolia@icloud.com>
2018-08-08 15:30:26 -0500

    Adjusted footer links

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,10 +3,10 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
-      <p><a href="#">Copyright 2018</a></p>
-      <p><a href="#">GitHub</a></p>
-      <p><a href="#">About</a></p>
-      <p><a href="#">Contact</a></p>
+      <p>Copyright 2018</p>
+      <p><a href="https://github.com/DKolia/FineGrind">GitHub</a></p>
+      <p><a href="/about">About</a></p>
+      <p><a href="/contact">Contact</a></p>
     </footer>
   )
 }
